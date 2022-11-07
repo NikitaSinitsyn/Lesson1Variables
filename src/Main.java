3da8b1033854d9dfb05e4fdcb0fa1181ae35fc88
NikitaSinitsyn <nikita.sinitsyn.lipetsk@abv.bg>
2022-11-07 20:59:30 +0200

    HW done!

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    public static void main(String[] args) {
+        //Exercise 1
+        double dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        //Exercise 2
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+        //Exercise 3
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+        //Exercise 4
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        //Exercise 5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        //Exercise 6
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var sumOfTHeWeights = firstBoxerWeight + secondBoxerWeight;
+        System.out.println(sumOfTHeWeights + " kg - total weight of two boxers!");
+        var differenceOfTheWeights = secondBoxerWeight - firstBoxerWeight;
+        System.out.println(differenceOfTheWeights + " kg - boxers weight difference!");
+
+        //Exercise 7
+        var firstMethodDifferenceOfTheWeight = secondBoxerWeight - firstBoxerWeight;
+        System.out.println(firstMethodDifferenceOfTheWeight);
+        var secondMethodDifferenceOfTheWeight = secondBoxerWeight % firstBoxerWeight;
+        System.out.println(secondMethodDifferenceOfTheWeight);
+
+        //Exercise 8
+        var totalWorkHours = 640;
+        var workHoursOfEmployees = 8;
+        var theNumberOfEmployees = totalWorkHours / workHoursOfEmployees;
+        System.out.println("Всего работников в компании – " + theNumberOfEmployees + " человек");
+        var numberOfEmployeesAfterChanges = theNumberOfEmployees + 94;
+        var workHoursOfEmployeesAfterChanges = totalWorkHours / numberOfEmployeesAfterChanges;
+        System.out.println("Если в компании работает " + numberOfEmployeesAfterChanges + " человек, то всего " + workHoursOfEmployeesAfterChanges + " часов работы может быть поделено между сотрудниками.");
+    }
+}
